e11de753d068ef5cd924950d900cf1bed81c6b87
Daniel Vargas <daniel.vg@globant.com>
2020-03-13 18:50:26 -0500

    Punto siete listo

---
+++ PuntoSiete/Main.php
@@ -0,0 +1,11 @@
+<?php
+$salpicon = array(
+    "dulce1" => "Banano",
+    "dulce2" => "Manzana",
+    "dulce3" => "Durazno",
+    "acido1" => "Piña",
+    "acido2" => "Naranja",
+    "acido3" => "Lulo"
+);
+print_r("El salpicón está compuesto por:\n");
+print_r($salpicon);
