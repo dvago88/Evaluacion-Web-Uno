61309dc156a37716dae8fd62241ed1f32d1962c5
Daniel Vargas <daniel.vg@globant.com>
2020-03-13 20:14:00 -0500

    Terminado punto uno

---
+++ PuntoUno/Main.php
@@ -0,0 +1,13 @@
+<?php
+$numero1 = 4;
+$numero2 = 5;
+
+$suma = $numero1 + $numero2;
+$resta = $numero1 - $numero2;
+$multiplicacion = $numero1 * $numero2;
+$division = $numero1 / $numero2;
+
+print_r("La suma de " . $numero1 . "+" . $numero2 . "=" . $suma . "\n");
+print_r("La resta de " . $numero1 . "-" . $numero2 . "=" . $resta . "\n");
+print_r("La multiplicación de " . $numero1 . "x" . $numero2 . "=" . $multiplicacion . "\n");
+print_r("La división de " . $numero1 . "/" . $numero2 . "=" . $division . "\n");
